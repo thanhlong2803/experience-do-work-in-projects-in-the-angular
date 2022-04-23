9b7b86e050c2576c3a16a4d7ad520f539387e38c
CaoThanhLong <thanhlong2803@hotmail.com>
2022-04-23 23:19:16 +0700

    Save info user in localStorage

--- experience-you-should-know/the-angular-set-get-localstorage-project/src/app/app-routing.module.ts
+++ experience-you-should-know/the-angular-set-get-localstorage-project/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BootrapAndLocalStorageComponent } from './bootstrap-localStorage/bootstrap-localStorage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: BootrapAndLocalStorageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
