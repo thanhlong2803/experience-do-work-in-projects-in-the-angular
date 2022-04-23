86c42081fb0497a526b81b44b089eac4d7504d41
CaoThanhLong <thanhlong2803@hotmail.com>
2022-04-23 23:19:16 +0700

    Save info user in localStorage

--- experience-you-should-know/the-angular-set-get-localstorage-project/src/app/app.module.ts
+++ experience-you-should-know/the-angular-set-get-localstorage-project/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BootrapAndLocalStorageComponent } from './bootstrap-localStorage/bootstrap-localStorage.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BootrapAndLocalStorageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
