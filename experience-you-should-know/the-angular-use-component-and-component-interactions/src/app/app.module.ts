894ab1075733bdf2a0aec2daafaa037a37919bb4
CaoThanhLong <thanhlong2803@hotmail.com>
2022-04-24 20:59:43 +0700

    done input and output

--- experience-you-should-know/the-angular-use-component-and-component-interactions/src/app/app.module.ts
+++ experience-you-should-know/the-angular-use-component-and-component-interactions/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { EmployeeShareComponent } from './component-interaction-input-and-output/employee-share/employee-share.component';
+import { CreateEmployeeComponent } from './component-interaction-input-and-output/create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeShareComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
